b3d3580f4525d5b301755a02d581d1901c2268cc
Jernej Vivod <je.vivod@gmail.com>
2020-01-01 15:26:33 +0100

    fix

--- test-data/generate_test_data.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import scipy.io as sio
-
-data = np.random.rand(100, 100)
-target = (data[:, 0] > data[:, 2]).astype(int)
-
-sio.savemat('data2.mat', {'data' : data})
-sio.savemat('target2.mat', {'target' : target})
-
